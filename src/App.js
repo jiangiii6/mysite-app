cc5b5d08e7c4a4d9f63e23c734bcd2ea983e4ebb
Jiang <38676115+jiangiii6@users.noreply.github.com>
2022-06-10 16:53:43 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React, { Component }  from 'react';
+import { Route, BrowserRouter as Routes, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Others from './components/Others';
+import './index.css';
 
+function App() {
+  return (
+    <Routes>
+    <main>
+     <Header />
+     <Home />  
+     </main>
+   <Others />
+    <Footer />
+    </Routes>
+  );
+}
+
+export default App;
